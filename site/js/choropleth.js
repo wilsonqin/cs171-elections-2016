bf1c9995aedde16fdb7d58cb5b0eb706cc89eb1b
James Curtin <jameswcurtin@gmail.com>
2016-04-08 19:45:52 -0400

    Get choropleth up and running without data (also don't have state boundaries)

--- site/js/choropleth.js
+++ site/js/choropleth.js
@@ -1,9 +1,9 @@
 // --> CREATE SVG DRAWING AREA
 var width = 960,
-    height = 600;
-
+    height = 500;
+var scale = 1000;
 var projection = d3.geo.albersUsa()
-    .scale(1280)
+    .scale(scale)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -20,31 +20,38 @@ loadData();
 function loadData() {
     // Use the Queue.js library to read two files
     queue()
-        .defer(d3.json, "data/us.json")
+        .defer(d3.json, "data/cb_2014_us_county_5m.json")
         // .defer(d3.csv, "data/global-malaria-2015_touse.csv")
-        .await(function(error, mapTopoJson, malariaDataCsv) {
+        .await(function(error, us, malariaDataCsv) {
             // --> PROCESS DATA
             // cleandata(malariaDataCsv);
             // Update choropleth
             // selection = d3.select("#ranking-type").property("value");
-            updateChoropleth(mapTopoJson);
+            updateChoropleth(us);
         });
 }
 
-function updateChoropleth(mapTopoJson) {
-    console.log(mapTopoJson);
-
+function updateChoropleth(us) {
+    console.log(us);
+    console.log(us.objects.cb_2014_us_county_5m);
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
-        .data(topojson.feature(mapTopoJson, mapTopoJson.objects.counties).features)
-        .enter().append("path")
-        // .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-        .attr("d", path);
-
-    svg.append("path")
-        .datum(topojson.mesh(mapTopoJson, mapTopoJson.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "states")
-        .attr("d", path);
-
+        .data(topojson.feature(us, us.objects.cb_2014_us_county_5m).features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .on("mouseover", function(d){
+            var boundaries = this.getBoundingClientRect();
+            d3.select("#tooltip")
+                .style("left", (boundaries.right + 10) + "px")
+                .style("top", (boundaries.top + 10) + "px");
+            d3.select("#county")
+                .text(d.properties.NAME);
+            d3.select("#tooltip")
+                .classed("hidden", false);
+        })
+        .on("mouseout", function(){
+            d3.select("#tooltip").classed("hidden", true);
+        });
 }
