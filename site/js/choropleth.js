b4f606a1dfaabe82086eb0e655e54df4503bf372
James Curtin <jameswcurtin@gmail.com>
2016-04-11 20:26:00 -0400

    Improve choropleth with zoom

--- site/js/choropleth.js
+++ site/js/choropleth.js
@@ -1,9 +1,9 @@
-// --> CREATE SVG DRAWING AREA
 var width = 960,
-    height = 500;
-var scale = 1000;
+    height = 500,
+    centered;
+
 var projection = d3.geo.albersUsa()
-    .scale(scale)
+    .scale(1070)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -15,14 +15,22 @@ var svg = d3.select("#choropleth1")
     .attr("height", height)
     .attr("id", "choropleth-svg");
 
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", clicked);
+
+var g = svg.append("g");
+
 loadData();
 
 function loadData() {
     // Use the Queue.js library to read two files
     queue()
-        .defer(d3.json, "data/cb_2014_us_county_5m.json")
+        .defer(d3.json, "data/us.json")
         // .defer(d3.csv, "data/global-malaria-2015_touse.csv")
-        .await(function(error, us, malariaDataCsv) {
+        .await(function(error, us) {
             // --> PROCESS DATA
             // cleandata(malariaDataCsv);
             // Update choropleth
@@ -31,27 +39,73 @@ function loadData() {
         });
 }
 
-function updateChoropleth(us) {
+function updateChoropleth(us){
     console.log(us);
-    console.log(us.objects.cb_2014_us_county_5m);
-    svg.append("g")
-        .attr("class", "counties")
+    g.append("g")
+        .attr("id", "counties")
         .selectAll("path")
-        .data(topojson.feature(us, us.objects.cb_2014_us_county_5m).features)
-        .enter()
-        .append("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter().append("path")
         .attr("d", path)
+        .attr("class", "county-boundary")
+        .on("click", countyclicked)
         .on("mouseover", function(d){
             var boundaries = this.getBoundingClientRect();
             d3.select("#tooltip")
                 .style("left", (boundaries.right + 10) + "px")
                 .style("top", (boundaries.top + 10) + "px");
             d3.select("#county")
-                .text(d.properties.NAME);
+                .text(d.id);
             d3.select("#tooltip")
                 .classed("hidden", false);
         })
         .on("mouseout", function(){
             d3.select("#tooltip").classed("hidden", true);
         });
+
+    g.append("g")
+        .attr("id", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("class", "state")
+        .on("click", clicked);
+
+
+    g.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("id", "state-borders")
+        .attr("d", path);
+};
+
+
+function clicked(d) {
+    console.log(d);
+    var x, y, k;
+
+    if (d && centered !== d) {
+        var centroid = path.centroid(d);
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+        centered = d;
+    } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        centered = null;
+    }
+
+    g.selectAll("path")
+        .classed("active", centered && function(d) { return d === centered; });
+
+    g.transition()
+        .duration(750)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .style("stroke-width", 1.5 / k + "px");
+}
+
+function countyclicked(d) {
+    alert(d.id);
 }
