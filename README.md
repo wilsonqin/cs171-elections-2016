4c9fa6e5300ac98935dda99bc8dab0dd6e7f8517
James Curtin <jameswcurtin@gmail.com>
2016-04-11 22:43:30 -0400

    Add rough zoom for to show individual state

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cs171-elections-2016
 Data Visualization Project for CS 171
+
+### Sources:
+
+http://techslides.com/demos/d3/us-zoom-county.html
--- site/js/choropleth.js
+++ site/js/choropleth.js
@@ -15,6 +15,12 @@ var svg = d3.select("#choropleth1")
     .attr("height", height)
     .attr("id", "choropleth-svg");
 
+var svg2 = d3.select("#choropleth2")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", "choropleth-svg2");
+
 svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
@@ -22,25 +28,38 @@ svg.append("rect")
     .on("click", clicked);
 
 var g = svg.append("g");
+var g2 = svg2.append("g");
 
 loadData();
-
+var testdata;
 function loadData() {
     // Use the Queue.js library to read two files
     queue()
         .defer(d3.json, "data/us.json")
+        .defer(d3.tsv, "data/us-state-names.tsv")
+        .defer(d3.tsv, "data/us-county-names.tsv")
         // .defer(d3.csv, "data/global-malaria-2015_touse.csv")
-        .await(function(error, us) {
+        .await(function(error, us, stateNames, countyNames) {
             // --> PROCESS DATA
             // cleandata(malariaDataCsv);
             // Update choropleth
             // selection = d3.select("#ranking-type").property("value");
-            updateChoropleth(us);
+            updateChoropleth(us, stateNames, countyNames);
         });
 }
 
-function updateChoropleth(us){
+function updateChoropleth(us, stateNames, countyNames) {
     console.log(us);
+    testdata = us;
+    var states = {};
+    var counties = {};
+    stateNames.forEach(function (d, i) {
+        states[d.id] = d.name;
+    });
+    countyNames.forEach(function (d, i) {
+        counties[d.id] = d.name;
+    });
+
     g.append("g")
         .attr("id", "counties")
         .selectAll("path")
@@ -49,17 +68,17 @@ function updateChoropleth(us){
         .attr("d", path)
         .attr("class", "county-boundary")
         .on("click", countyclicked)
-        .on("mouseover", function(d){
+        .on("mouseover", function (d) {
             var boundaries = this.getBoundingClientRect();
             d3.select("#tooltip")
                 .style("left", (boundaries.right + 10) + "px")
                 .style("top", (boundaries.top + 10) + "px");
             d3.select("#county")
-                .text(d.id);
+                .text("County: " + counties[d.id]);
             d3.select("#tooltip")
                 .classed("hidden", false);
         })
-        .on("mouseout", function(){
+        .on("mouseout", function () {
             d3.select("#tooltip").classed("hidden", true);
         });
 
@@ -70,18 +89,35 @@ function updateChoropleth(us){
         .enter().append("path")
         .attr("d", path)
         .attr("class", "state")
-        .on("click", clicked);
+        .on("click", clicked)
+        .on("click", gennewstate)
+        .on("mouseover", function (d) {
+            var boundaries = this.getBoundingClientRect();
+            d3.select("#tooltip")
+                .style("left", (boundaries.right + 10) + "px")
+                .style("top", (boundaries.top + 10) + "px");
+            d3.select("#county")
+                .text(states[d.id]);
+            d3.select("#tooltip")
+                .classed("hidden", false);
+        })
+        .on("mouseout", function () {
+            d3.select("#tooltip").classed("hidden", true);
+        });
 
 
     g.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .datum(topojson.mesh(us, us.objects.states, function (a, b) {
+            return a !== b;
+        }))
         .attr("id", "state-borders")
         .attr("d", path);
-};
+
+}
 
 
 function clicked(d) {
-    console.log(d);
+    // console.log(d);
     var x, y, k;
 
     if (d && centered !== d) {
@@ -109,3 +145,32 @@ function clicked(d) {
 function countyclicked(d) {
     alert(d.id);
 }
+
+function gennewstate(datum) {
+    var states = topojson.feature(testdata, testdata.objects.states),
+        state = states.features.filter(function(d) {return d.id === datum.id; })[0];
+    console.log(state);
+    svg2.selectAll('path').remove();
+    svg2.append("path")
+        .datum(state)
+        .attr("d", path)
+        .attr("class", "state")
+        .attr("id", "focusstate");
+
+    var focusState = d3.select("#focusstate");
+    var x, y, k;
+    var centroid = path.centroid(datum);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = focusState;
+
+
+    svg2.selectAll("path")
+        .classed("active", centered && function(d) { return d === centered; });
+
+    focusState.transition()
+        .duration(750)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .style("stroke-width", 1.5 / k + "px");
+};
