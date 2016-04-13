67fcedb3e7d51836426d8d2261270f77140b2da3
Wilson Qin <wilson.qin@gmail.com>
2016-04-13 15:04:40 -0400

    format data script progress point for viz1 data

--- data/get_huff_polls.sh
+++ data/get_huff_polls.sh
@@ -2,6 +2,6 @@ COMMAND="/usr/bin/curl";
 COMMAND="$COMMAND http://elections.huffingtonpost.com/pollster/api/polls.json"
 COMMAND="$COMMAND?topic=2016-president-gop-primary&page=";
 
-END=1;
+END=99;
 
 for i in $(seq 1 $END); do $($COMMAND$i > huff-poll-page-$i.json); done
