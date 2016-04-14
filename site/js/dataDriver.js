05fa9abc854c8c599438166329f572c7daef7e73
Wilson Qin <wilson.qin@gmail.com>
2016-04-14 15:47:07 -0400

    dataDriver connected to vis1, vis1.chloro2 demo population works

--- site/js/dataDriver.js
+++ site/js/dataDriver.js
@@ -1,3 +1,8 @@
+/* 
+ * Serves as the data import driver
+ * and does initial first level d3 data manipulation
+ */
+
 (function(){
   if(!d3) console.log("we need d3 before dataDriver!");
   if(!queue) console.log("we need d3-queue before dataDriver!");
@@ -7,30 +12,122 @@
 
   var datasetReady = $.Deferred();
 
+  // a jQuery Deferred synchronization variable available globally
+  // this is resolved in the queue callback when all data is ready
+  window.dataReady = window.dataReady ? window.dataReady : datasetReady;
+
   // load data files async and converge
   queue()
     .defer(d3.json, "data/county_facts.json")
     .defer(d3.json, "data/us.json")
+    .defer(d3.json, "data/primary_results.json")
     .defer(d3.tsv, "data/us-state-names.tsv")
     .defer(d3.tsv, "data/us-county-names.tsv")
-    .await(function(err, countyFacts, usTOPOJSON, stateNames, countyNames){
+    .await(function(err, countyFacts, usTOPOJSON, primaryResults, stateNames, countyNames){
 
       var factMap = d3.map(countyFacts, function(d){ return d.fips; });
+
+      // init state and county name structures
 
       // TODO
       // Add a map similar to factMap that takes a state ID and returns a list of county objects
 
+      // primaryResults groupBy fips then party
+      var primaryResultsFipsParty = d3.nest()
+        .key(function(d){ return d.fips; })
+        .key(function(d){ return d.party; })
+        .sortValues(function descending(a, b) {
+          a = a.votes;
+          b = b.votes;
+          return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
+        })
+        .map(primaryResults, d3.map);
+
+      // primaryResults groupBy state_abbreviation, fips, party
+      var primaryResultsStateFipsParty = d3.nest()
+        .key(function(d){ return d.state_abbreviation; })
+        .key(function(d){ return d.fips; })
+        .key(function(d){ return d.party; })
+        .sortValues(function descending(a, b) {
+          a = a.votes;
+          b = b.votes;
+          return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
+        })
+        .map(primaryResults, d3.map);
+
       dataset = {
         countyFacts: factMap,
+        primaryResults: primaryResultsStateFipsParty,
+        primaryResultsFipsParty: primaryResultsFipsParty,
         stateNames: stateNames,
         countyNames: countyNames,
         usTOPOJSON: usTOPOJSON,
+        getCountyData: getCountyData
       };
 
-      window.data = window.data ? window.data : dataset;
+      usTOPOJSON = populateTopoAttr(usTOPOJSON, stateNames, countyNames, factMap);
 
+
+      window.vis1 = {
+        topoJSONdata: usTOPOJSON,
+        getCountyData: getCountyData,
+        getStateData: getStateData,
+        stateNames: stateNames,
+        countyNames: countyNames
+      };
+
+      // attach completed dataset to the window for global access
+      // window.data = window.data ? window.data : dataset;
+
+      // signal that the global data is ready to be accessed
       datasetReady.resolve();
     });
+  
+  /********** DATA MODEL METHODS *********/
+  /* pre-req: these should only be called when window.dataReady is resolved! **/
 
-  window.dataReady = window.dataReady ? window.dataReady : datasetReady;
+  // Enter FIPS code and get object with data for the selected county
+  function getCountyData(fips){
+    var fips_data = dataset.countyFacts.get(fips);
+    var getCountyResults = dataset.primaryResultsFipsParty.get(fips);
+    var partyMap = getCountyResults ? getCountyResults : d3.map([]);
+
+    return {
+      census: fips_data,
+      election: partyMap
+    };
+  }
+
+  // TODO
+  // Enter State ID and get list of county objects associated with the state
+  function getStateData(stateCode){
+    
+    var state_data = dataset.primaryResults.get(stateCode);
+    return state_data;
+  }
+
+  
+  function populateTopoAttr(usTOPOJSON, stateNames, countyNames, countyFacts){
+    var stateById = d3.map(stateNames, function(d){
+      return d.id;
+    });
+
+    var countyNameById = d3.map(countyNames, function(d){
+      return d.id;
+    });
+
+    usTOPOJSON.objects.states.geometries.forEach(function(state,i){
+      state.properties = stateById.get(state.id);
+    });
+
+    usTOPOJSON.objects.counties.geometries.forEach(function(county,i){
+      county.properties = {};
+      $.extend(county.properties, getCountyData(county.id), countyNameById.get(county.id));
+    });
+
+    return usTOPOJSON;
+  }
+
+
 }());
+
--- site/js/maps.js
+++ site/js/maps.js
@@ -6,8 +6,10 @@ var width1 = $("#choropleth1").parent().width(),
 // Create a number of global variables
 var centered,
     topoJSONdata,
-    stateList = {},
-    countiesList = {};
+    focusState,
+    focusCounty,
+    focusStateFacts,
+    lastCountyColor;
 
 // Create projection for map of USA
 var projection = d3.geo.albersUsa()
@@ -44,6 +46,8 @@ var tooltip = d3.select("body").append("div")
 var g = svg.append("g"),
     g2 = svg2.append("g");
 
+// data to be populated by loadData
+//var data;
 
 // Create variables that hold values of selected elements
 var selectedDemographicVal = "",
@@ -58,42 +62,26 @@ loadData();
 
 function loadData() {
     $.when(window.dataReady).then(function(){
-        // console.log('data ready in chloro');
-        var data = window.data;
-        console.log(data);
+        if(!vis1 || !window.vis1) console.log("error: dataDriver not intialized before maps.js");
 
-        topoJSONdata = data.usTOPOJSON;
-        // console.log(topoJSONdata);
-
-        mapNames(data.stateNames, data.countyNames);
+        topoJSONdata = vis1.topoJSONdata;
 
         updateChoropleth(topoJSONdata);
     });
-
-    // console.log('data not yet ready in chloro');
 }
 
-// Enter FIPS code and get object with data for the selected county
-function getCountyData(fips){
-    var fips_data = data.countyFacts.get(fips);
-    return fips_data;
-}
+/* 
+* returns county-by-county results for a state and party
+*
+* params:
+*  -fipsCode (int or string)
+*  -party (string)
+*/
+function getResultsByState(stateCode){
+// TODO: validation that stateCode is valid
 
-// TODO
-// Enter State ID and get list of county objects associated with the state
-function getStateData(stateCode){
-    var state_data = data.stateFacts.get(stateCode);
-    return state_data;
-}
-
-function mapNames(stateNames, countyNames){
-
-    stateNames.forEach(function (d) {
-        stateList[d.id] = d.name;
-    });
-    countyNames.forEach(function (d) {
-        countiesList[d.id] = d.name;
-    });
+var results = window.primaryResults.get(stateCode);
+return results;
 }
 
 function updateChoropleth(us) {
@@ -160,18 +148,24 @@ function clicked(d) {
 }
 
 function countyclicked(d) {
-    console.log(getCountyData(d.id));
+    console.log('getCountyData: ', d.properties);
     alert(d.id);
 }
 
 function showCountyTooltip (d) {
+    // track former county color to restore when tooltip leaves
+    lastCountyColor = $("#" + d.id + "inset").css("fill");
+
     $("#" + d.id).css("fill", "orange");
     $("#" + d.id + "inset").css("fill", "orange");
+
+    var results = d.properties.election.get(selectedParty.val());
+    var winner = results ? results[0].candidate : "No winner / Data Missing";
 
     tooltip.transition()
         .duration(200)
         .style("opacity", .9);
-    tooltip.html("County: " + countiesList[d.id])
+    tooltip.html("<p>County: " + d.properties.name +"</p>" + "<p>"+ winner +"</p>")
         .style("left", (d3.event.pageX) +25 + "px")
         .style("top", (d3.event.pageY - 40) + "px");
 }
@@ -180,14 +174,14 @@ function showStateTooltip (d) {
     tooltip.transition()
         .duration(200)
         .style("opacity", .9);
-    tooltip.html(stateList[d.id])
+    tooltip.html(d.properties.name)
         .style("left", (d3.event.pageX) +25 + "px")
         .style("top", (d3.event.pageY - 40) + "px");
 }
 
 function hideTooltip(d) {
     $("#" + d.id).css("fill", "#aaa");
-    $("#" + d.id + "inset").css("fill", "lightgrey");
+    $("#" + d.id + "inset").css("fill", lastCountyColor);
     tooltip.transition()
         .duration(500)
         .style("opacity", 0);
@@ -200,35 +194,36 @@ function genNewState(d) {
         selectedDemographicVal = selectedDemographic.val();
     }
 
-    // TODO
-    // var stateFacts = getStateData(d.id);
-    // var extent = d3.extent(stateFacts, function(d) { return +d[selectedVal]; });
-    // var quantize = d3.scale.quantize()
-    //     .domain(extent)
-    //     .range(colorbrewer.Blues["8"]);
+    focusState = d.properties.code;
 
-    var states = topojson.feature(topoJSONdata, topoJSONdata.objects.states),
-        state = states.features.filter(function(datum) {return datum.id === d.id; })[0];
-    var allCounties = topojson.feature(topoJSONdata, topoJSONdata.objects.counties),
-        filterCounties = allCounties.features.filter(function(datum) {
-            if (String(d.id).length === 1){
-                if(String(datum.id).length === 4) {
-                    return String(datum.id).substring(0, 1) === String(d.id);
-                }
+    var allCounties = topojson.feature(topoJSONdata, topoJSONdata.objects.counties);
+    var filterCounties = allCounties.features.filter(function(datum) {
+            if(!datum.properties.census){
+                // counties that we don't have census data for
+
+                //console.log(datum);
+                return false;
             }
-            else{
-                return String(datum.id).substring(0, 2)=== String(d.id);
-            }
+
+            return datum.properties.census.state_abbreviation === focusState;
         });
+
+    // Calculate domain for the selected demographic census property for state's counties
+    // Then we set a color scale
+    var extent = d3.extent(filterCounties, function(d) { return d.properties.census[selectedDemographic.val()]; });
+    var quantize = d3.scale.linear()
+        .domain(extent)
+        .range(colorbrewer.Blues[8]);
+
     g2.selectAll('path').remove();
     g2.selectAll('g').remove();
     svg2.selectAll('text').remove();
 
     var stateText = $("#stateText");
-    stateText.text(stateList[d.id]);
+    stateText.text(d.properties.name);
 
     g2.append("path")
-        .datum(state)
+        .datum(d)
         .attr("d", path)
         .attr("class", "state")
         .attr("id", "focusState");
@@ -238,10 +233,12 @@ function genNewState(d) {
         .selectAll("path")
         .data(filterCounties)
         .enter().append("path")
-        // .attr("class", function(d) { return quantize(stateFacts[d.id][selectedDemographicVal]); })
+        .attr("fill", function(d) {
+            return d.properties.census ? quantize(d.properties.census[selectedDemographic.val()]) : "gray"; 
+        })
         .attr("d", path)
         .attr("id", function (d) {return String(d.id) + "inset";})
-        .attr("class", "countyInset")
+        //.attr("class", "countyInset")
         .on("mouseover", showCountyTooltip)
         .on("mouseout", hideTooltip)
         .on("click", countyclicked);
